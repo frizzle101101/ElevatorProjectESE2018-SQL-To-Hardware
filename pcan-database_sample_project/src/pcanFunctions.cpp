8b17e9a7f0dd7228f9b214049e7a67b4da220653
frizzle101101 <aaronfritz96@gmail.com>
2018-07-09 14:10:22 -0400

    -Starting conversdion to oop with creating the pCanObj class, needs further oop implementation improvments -Creating Supivisor controler (SC) mode for pcan rx and tx from website and hardware

--- pcan-database_sample_project/src/pcanFunctions.cpp
+++ pcan-database_sample_project/src/pcanFunctions.cpp
@@ -12,62 +12,92 @@
 #include <ctype.h>
 #include <libpcan.h>   					// PCAN library
 
+class PCanObj
+{
+public:
+	int pcanTx(int id, int data){
+		h = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);		// Open PCAN channel
 
-// Globals
-// ***********************************************************************************************************
-HANDLE h;
-HANDLE h2;
-TPCANMsg Txmsg;
-TPCANMsg Rxmsg;
-DWORD status;
+		// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
+		status = CAN_Init(h, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
 
-// Code
-// ***********************************************************************************************************
+		// Clear the channel - new - Must clear the channel before Tx/Rx
+		status = CAN_Status(h);
 
-// Functions
-// *****************************************************************
-int pcanTx(int id, int data){
-	h = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);		// Open PCAN channel
+		// Set up message
+		Txmsg.ID = id;
+		Txmsg.MSGTYPE = MSGTYPE_STANDARD;
+		Txmsg.LEN = 1;
+		Txmsg.DATA[0] = data;
 
-	// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
-	status = CAN_Init(h, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
+		sleep(1);
+		status = CAN_Write(h, &Txmsg);
 
-	// Clear the channel - new - Must clear the channel before Tx/Rx
-	status = CAN_Status(h);
+		// Close CAN 2.0 channel and exit
+		CAN_Close(h);
+	}
 
-	// Set up message
-	Txmsg.ID = id;
-	Txmsg.MSGTYPE = MSGTYPE_STANDARD;
-	Txmsg.LEN = 1;
-	Txmsg.DATA[0] = data;
+	int pcanRxN(int num_msgs){
+		int i = 0;
 
-	sleep(1);
-	status = CAN_Write(h, &Txmsg);
+		// Open a CAN channel
+		h2 = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);
 
-	// Close CAN 2.0 channel and exit
-	CAN_Close(h);
-}
+		// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
+		status = CAN_Init(h2, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
 
-int pcanRx(int num_msgs){
-	int i = 0;
+		// Clear the channel - new - Must clear the channel before Tx/Rx
+		status = CAN_Status(h2);
 
-	// Open a CAN channel
-	h2 = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);
+		// Clear screen to show received messages
+		system("@cls||clear");
 
-	// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
-	status = CAN_Init(h2, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
+		// receive CAN message  - CODE adapted from PCAN BASIC C++ examples pcanread.cpp
+		printf("\nReady to receive message(s) over CAN bus\n");
 
-	// Clear the channel - new - Must clear the channel before Tx/Rx
-	status = CAN_Status(h2);
+		// Read 'num' messages on the CAN bus
+		while(i < num_msgs) {
+			while((status = CAN_Read(h2, &Rxmsg)) == PCAN_RECEIVE_QUEUE_EMPTY){
+				sleep(1);
+			}
+			if(status != PCAN_NO_ERROR) {						// If there is an error, display the code
+				printf("Error 0x%x\n", (int)status);
+				//break;
+			}
 
-	// Clear screen to show received messages
-	system("@cls||clear");
+			if(Rxmsg.ID != 0x01 && Rxmsg.LEN != 0x04) {		// Ignore status message on bus
+				printf("  - R ID:%4x LEN:%1x DATA:%02x \n",	// Display the CAN message
+					(int)Rxmsg.ID,
+					(int)Rxmsg.LEN,
+					(int)Rxmsg.DATA[0]);
+			i++;
+			}
+		}
 
-	// receive CAN message  - CODE adapted from PCAN BASIC C++ examples pcanread.cpp
-	printf("\nReady to receive message(s) over CAN bus\n");
 
-	// Read 'num' messages on the CAN bus
-	while(i < num_msgs) {
+		// Close CAN 2.0 channel and exit
+		CAN_Close(h2);
+		//printf("\nEnd Rx\n");
+		return ((int)Rxmsg.DATA[0]);						// Return the last value received
+	}
+
+	void pcanExecuteRecievedCommand(){
+		// Open a CAN channel
+		h2 = LINUX_CAN_Open("/dev/pcanusb32", O_RDWR);
+
+		// Initialize an opened CAN 2.0 channel with a 125kbps bitrate, accepting standard frames
+		status = CAN_Init(h2, CAN_BAUD_125K, CAN_INIT_TYPE_ST);
+
+		// Clear the channel - new - Must clear the channel before Tx/Rx
+		status = CAN_Status(h2);
+
+		// Clear screen to show received messages
+		system("@cls||clear");
+
+		// receive CAN message  - CODE adapted from PCAN BASIC C++ examples pcanread.cpp
+		printf("\nReady to receive message(s) over CAN bus\n");
+
+		// Read 'num' messages on the CAN bus
 		while((status = CAN_Read(h2, &Rxmsg)) == PCAN_RECEIVE_QUEUE_EMPTY){
 			sleep(1);
 		}
@@ -76,18 +106,31 @@ int pcanRx(int num_msgs){
 			//break;
 		}
 
-		if(Rxmsg.LEN != 0x04) {		// Ignore status message on bus	
+		if(Rxmsg.ID != 0x01 && Rxmsg.LEN != 0x04 ) {		// Ignore status message on bus
 			printf("  - R ID:%4x LEN:%1x DATA:%02x \n",	// Display the CAN message
 				(int)Rxmsg.ID,
 				(int)Rxmsg.LEN,
 				(int)Rxmsg.DATA[0]);
-		i++;
+			if(Rxmsg.ID == ID_CC_TO_SC)
+			{
+				pcanTx(ID_SC_TO_EC, HexFromFloor(floorNumber));
+			}
+			else if(Rxmsg.ID == ID_F1_TO_SC)
+			{
+				pcanTx(ID_SC_TO_EC, GO_TO_FLOOR1);
+			}
+			else if(Rxmsg.ID == ID_F2_TO_SC)
+			{
+				pcanTx(ID_SC_TO_EC, GO_TO_FLOOR2);
+			}
+			else if(Rxmsg.ID == ID_F3_TO_SC)
+			{
+				pcanTx(ID_SC_TO_EC, GO_TO_FLOOR3);
+			}
 		}
+
+
+		// Close CAN 2.0 channel and exit
+		CAN_Close(h2);
 	}
-
-
-	// Close CAN 2.0 channel and exit
-	CAN_Close(h2);
-	//printf("\nEnd Rx\n");
-	return ((int)Rxmsg.DATA[0]);						// Return the last value received
 }
